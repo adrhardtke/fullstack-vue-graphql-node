82e00c0d6551143e7b30ec5cb024095f854bbcb8
adriano hardtke <adr.hardtke@gmail.com>
2020-09-17 23:45:42 -0300

    create index page

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,27 @@
               <div class="card-body">
                 <ul class="list-group">
                   <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                    {{prefix}}
+                    <div class="row">
+                      <div class="col-md">
+                        {{prefix}}                        
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deletePrefix(prefix)">
+                          <i class="fa fa-trash"></i>
+                        </button>     
+                      </div>
+                    </div>
                   </li>
                 </ul>
                 <br/>
-                <input class="form-control" type="text" placeholder="Digite o prefixo" />
+                <div class="input-group">
+                  <input class="form-control" v-model="prefix" v-on:keyup.enter="addPrefix(prefix)" type="text" placeholder="Digite o prefixo" />
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addPrefix(prefix)">
+                      <i class="fa fa-plus"></i>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -29,11 +45,27 @@
               <div class="card-body">
                 <ul class="list-group">
                   <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                    {{sufix}}
+                    <div class="row">
+                      <div class="col-md">
+                        {{sufix}}                        
+                      </div>
+                      <div class="col-md text-right">
+                        <button class="btn btn-info" v-on:click="deleteSufix(sufix)">
+                          <i class="fa fa-trash"></i>
+                        </button>     
+                      </div>
+                    </div>
                   </li>
                 </ul>
                 <br/>
-                <input class="form-control" type="text" placeholder="Digite o sufixo" />
+                <div class="input-group">
+                  <input class="form-control" v-model="sufix" v-on:keyup.enter="addSufix(prefix)" type="text" placeholder="Digite o sufixo" />
+                  <div class="input-group-append">
+                    <button class="btn btn-info" v-on:click="addSufix(sufix)">
+                      <i class="fa fa-plus"></i>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -45,7 +77,16 @@
           <div class="card-body">
             <ul class="list-group">
               <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
-                {{domain}}
+                <div class="row">
+                  <div class="col-md">
+                    {{domain.name}}
+                  </div>
+                  <div class="col-md text-right">
+                    <a class="btn btn-info" :href="domain.checkout" target="_blank">
+                      <i class="fa fa-shopping-cart"></i>
+                    </a>
+                  </div>
+                </div>
               </li>
             </ul>
           </div>
@@ -60,14 +101,50 @@ import "bootstrap/dist/css/bootstrap.css"
 import "font-awesome/css/font-awesome.css"
 
 export default {
-  name: 'App',
-  data: function(){
-    return {
-      prefixes: ['Air', 'Jet', 'Flight'],
-      sufixes: ['Hub', 'Station', 'Mart'],
-      domains: ['AirHub', 'AirStation', 'AirMart', 'JetHub', 'JetStation', 'JetMart', 'FlightHub', 'FlightStation', 'FlightMart']
-    }
-  }
+	name: "App",
+	data: function(){
+		return {
+			prefix: "",
+			sufix: "",
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Mart"]
+		}
+	},
+	methods: {
+		addPrefix(prefix){
+			this.prefixes.push(prefix)
+			this.prefix = ""
+		},
+		addSufix(sufix){
+			this.sufixes.push(sufix)
+			this.sufix = ""
+		},		
+		deletePrefix(prefix){
+			const index = this.prefixes.indexOf(prefix)
+			this.prefixes.splice(index,1)
+		},
+		deleteSufix(sufix){
+			const index = this.sufixes.indexOf(sufix)
+			this.prefixes.splice(index,1)
+		},		
+	},
+	computed: {
+		//Computed properties, possuem watchers que só executarão novamente, quando as variaveis que ela depende, no casso
+		// de domains, as variaveis são prefixes e sufixes, quando elas mudarem, o codigo domains() é executado
+		domains(){
+			console.log("Generating domains...")
+			const domains = []
+			for(const prefix of this.prefixes){
+				for (const sufix of this.sufixes){
+					const name = prefix + sufix
+					const url = name.toLowerCase()
+					const checkout = `http://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`
+					domains.push({name, checkout})
+				}
+			}
+			return domains
+		}
+	}
 }
 </script>
 
